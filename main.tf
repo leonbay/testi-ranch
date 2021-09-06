7da7f6ace2a9934cd0a5370f918c31bb76dcd7dc
Tiina Antikainen <tiina.antikainen@awacademyconsultant.fi>
2021-09-06 15:50:11 +0300

    first version

---
+++ main.tf
@@ -0,0 +1,52 @@
+terraform {
+  backend "gcs" {
+    bucket  = var.backend_bucket
+    prefix  = "terraform/state"
+  }
+
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "~> 3.45.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+}
+
+module "storage" {
+  source     = ".//modules/storage"
+  project_id = var.project_id
+  region = var.region
+}
+
+module "database" {
+  source = ".//modules/database"
+  project_id = var.project_id
+  region = var.region
+}
+
+module "functions" {
+  source = ".//modules/functions"
+  project_id = var.project_id
+  region = var.region
+}
+
+module "data_fusion" {
+  source = ".//modules/data_fusion"
+  project_id = var.project_id
+  region = var.region
+}
+
+module "cloud_build" {
+  source = ".//modules/cloud_build"
+  project_id = var.project_id
+  region = var.region
+}
+
+module "cloud_scheduler" {
+    source = ".//modules/cloud_build"
+    project_id = var.project_id
+}
