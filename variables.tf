afc796a384c3e9be8338df740ecda8a55f00a9b3
Tiina Antikainen <tiina.antikainen@awacademyconsultant.fi>
2021-09-07 13:10:47 +0300

    main changes as well as variable

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-a"
+}
+
+variable "project" {
+  type        = string
+  default     = "leo-test-env-1"
+  description = "description"
+}
