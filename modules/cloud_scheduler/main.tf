35740f2db097fe3ec6e615b0df25d93e25f47fe5
Lahenleo <lahtileo.o@gmail.com>
2021-09-07 10:36:32 +0300

    cloud scheduler pohja

---
+++ modules/cloud_scheduler/main.tf
@@ -0,0 +1,12 @@
+resource "google_cloud_scheduler_job" "job" {
+  name             = var.scheduler_name
+  description      = var.scheduler_description
+  schedule         = var.schedule
+  time_zone        = var.time_zone
+  
+
+  http_target {
+    http_method = var.http_method
+    uri         = module.functions.function_url #tämä ei välttämättä oikein
+  }
+}
