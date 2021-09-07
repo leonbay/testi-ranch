4cae1ea8e58421e797a6b667bacd60d742616fc7
Tiina Antikainen <tiina.antikainen@awacademyconsultant.fi>
2021-09-07 13:14:42 +0300

    main changes as well as variable

--- main.tf
+++ main.tf
@@ -1,19 +1,16 @@
 terraform {
-  backend "gcs" {
-    bucket  = var.backend_bucket
-    prefix  = "terraform/state"
-  }
-
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "~> 3.45.0"
+      version = "~> 3.5.0"
     }
   }
 }
 
 provider "google" {
-  credentials = file(var.credentials_file)
+  project= var.project
+  region = var.region
+  zone = var.zone
 }
 
 module "storage" {
