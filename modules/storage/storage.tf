4ee3f92955bc656c7ab70c25ff46453c8b24fa57
Lahenleo <lahtileo.o@gmail.com>
2021-09-10 15:36:42 +0300

    hipsut

--- modules/storage/storage.tf
+++ modules/storage/storage.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "bucket" {
   name          = var.name[count.index]
-  storage_class = MULTI_REGIONAL
+  storage_class = "MULTI_REGIONAL"
   force_destroy = true
   project = var.project
   count = 2
