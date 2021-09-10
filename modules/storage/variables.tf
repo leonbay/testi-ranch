b9c3567b2133eeaca57bf62acac44932c3d828f5
gitmikko <mikontreeniacco@gmail.com>
2021-09-10 14:18:00 +0300

    eka kokeilu apply v.15

--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -1,6 +1,6 @@
 variable "name" {
   type        = list(string)
-  default     = ["backend-bucket-final-project","cloud-function-final-project", "currency-raw-data-json-test"]
+  default     = ["cloud-function-final-project", "currency-raw-data-json-test"]
   description = "description"
 }
 
