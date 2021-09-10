4d43b3261c6ceddda9659ca068d4b122ebee322e
Lahenleo <lahtileo.o@gmail.com>
2021-09-10 15:53:56 +0300

    säätö#

--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -1,6 +1,6 @@
 variable "name" {
   type        = list(string)
-  default     = ["cloud-function-final-project", "currency-raw-data-json-test", "currency_csv_bucket"]
+  default     = ["cloud-function-final-project", "currency-raw-data-json-test", "currency_csv_bucket-test"]
   description = "description"
 }
 
