8be822fa7cb7e7dc49ae2d9e5855e18258b2f487
gitmikko <mikontreeniacco@gmail.com>
2021-09-10 13:27:47 +0300

    eka kokeilu apply v.8

--- modules/storage/storage.tf
+++ modules/storage/storage.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "bucket" {
   name          = var.name[count.index]
   location      = var.region
-  storage_class = MULTI_REGIONAL
+  storage_class = MULTI_REGIONAL[count.index]
   force_destroy = true
   project = var.project
   count = 3
