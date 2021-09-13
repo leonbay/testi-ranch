992e87acce1e5bd6e1b1b2c534c6db590a298c9e
ilkka <ilkka.o.pekkala@gmail.com>
2021-09-13 11:28:40 +0300

    Added timestamp functionality to dialogflowfunction

--- Dialogfunction/main.py
+++ Dialogfunction/main.py
@@ -4,16 +4,16 @@ client = bigquery.Client()
 
 def dialogfunction(request):
     import json
-    geocountry = "Not found"
+
     content_type = request.headers['content-type']
     if content_type == 'application/json':
         request_json = request.get_json(silent=True)
         geocountry = request_json['queryResult']['parameters']['geo-country']
+        datetime = request_json['queryResult']['parameters']['date-time']
 
-    myresp = "Not found"
-    query = f"SELECT per100k FROM `loppuprojekti-325208.ML_test_1.ML_global_forecast_results_with_per100k` WHERE location = '{geocountry}' ORDER BY per100k DESC LIMIT 1"
+    query = f"SELECT per100k FROM `loppuprojekti-325208.ML_test_1.ML_global_forecast_results_with_per100k` WHERE location = '{geocountry}' AND forecast_timestamp = TIMESTAMP_TRUNC('{datetime}', DAY) LIMIT 1"
     query_job = client.query(query) 
     for row in query_job:
-        myresp = str(row[0])
-    
-    return json.dumps({'fulfillmentText': myresp})
+        myresp = str(round(row[0],2))
+    finalresponse = f"The forecast for new cases in {geocountry} on {datetime} is {myresp}"
+    return json.dumps({'fulfillmentText': finalresponse})
