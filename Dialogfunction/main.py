612c623380d88608d9957f13577481878dc1e2b7
ilkka <ilkka.o.pekkala@gmail.com>
2021-09-13 10:33:06 +0300

    Adding geocountry header

--- Dialogfunction/main.py
+++ Dialogfunction/main.py
@@ -2,15 +2,17 @@ from google.cloud import bigquery
 
 client = bigquery.Client()
 
-#Example function json payload required:
 def dialogfunction(request):
-    #request_json = request.get_json(silent=True)
-    #approxlongitude = request_json.get("longitude")
-    #approxlatitude = request_json.get("latitude")
+    geocountry = "Not found"
+    content_type = request.headers['content-type']
+    if content_type == 'application/json':
+        request_json = request.get_json(silent=True)
+        if request_json and 'geo-country' in request_json:
+            geocountry = request_json['geo-country']
 
     myresp = "Not found"
     query = f"SELECT per100k FROM `loppuprojekti-325208.ML_test_1.ML_global_forecast_results_with_per100k` WHERE location = 'Albania' ORDER BY per100k DESC LIMIT 1"
     query_job = client.query(query) 
     for row in query_job:
-        myresp = row[0]
-    return myresp
+        myresp = str(row[0])
+    return geocountry
