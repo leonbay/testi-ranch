efb5f62c58fd2ff29107d150a693a4deb071d60d
gitmikko <mikontreeniacco@gmail.com>
2021-09-14 15:04:24 +0300

    lisätty pari bucketia

--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -1,6 +1,6 @@
 variable "name" {
   type        = list(string)
-  default     = ["cloud-function-final-project", "currency-raw-data-json", "currency_csv_bucket"]
+  default     = ["cloud-function-final-project", "currency-raw-data-json", "currency_csv_bucket", "covid-archive", "covid-raw-data"]
   description = "description"
 }
 
