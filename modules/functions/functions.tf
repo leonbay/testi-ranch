1f003ed7652e8a3c3084f1dfa1a3bfb014def967
gitmikko <mikontreeniacco@gmail.com>
2021-09-15 09:27:34 +0300

    hiottu triggereitä

--- modules/functions/functions.tf
+++ modules/functions/functions.tf
@@ -4,13 +4,32 @@ resource "google_cloudfunctions_function" "function" {
   name        = var.function_name[count.index]
   runtime     = var.runtime
   available_memory_mb   = 256
-  count = 8
+  count = 6
   trigger_http          = true
   entry_point           = var.entrypoint[count.index]
   service_account_email = var.service_account
 
   source_repository {
     url = var.source_repository_url[count.index]
+  }
+}
+
+resource "google_cloudfunctions_function" "function2" {
+  project = var.project
+  region = var.region
+  name        = var.function_name2[count.index]
+  runtime     = var.runtime
+  available_memory_mb   = 256
+  count = 2
+  event_trigger {
+    event_type = "google.storage.object.finalize"
+    resource = "covid-raw-data"
+  }
+  entry_point           = var.entrypoint2[count.index]
+  service_account_email = var.service_account
+
+  source_repository {
+    url = var.source_repository_url2[count.index]
   }
 }
 
--- modules/functions/variables.tf
+++ modules/functions/variables.tf
@@ -5,7 +5,12 @@ variable "runtime" {
 
 variable "entrypoint" {
   type = list(string)
-  default = ["upload_to_bq","currency_history", "delete_blob", "currency_today", "currenttohistory", "history", "dataprep_job_gcs_trigger", "dataprep_job_gcs_trigger"]
+  default = ["upload_to_bq","currency_history", "delete_blob", "currency_today", "currenttohistory", "history"]
+}
+
+variable "entry_point2" {
+  type = list(string)
+  default = ["dataprep_job_gcs_trigger", "dataprep_job_gcs_trigger"]
 }
 
 variable "source_repository_url" {
@@ -15,14 +20,23 @@ variable "source_repository_url" {
              "https://source.developers.google.com/projects/leo-test-env-1/repos/github_leonbay_testi-ranch/moveable-aliases/master/paths/functions/delete-func",
              "https://source.developers.google.com/projects/leo-test-env-1/repos/github_leonbay_testi-ranch/moveable-aliases/master/paths/functions/todayscurrencies",
              "https://source.developers.google.com/projects/leo-test-env-1/repos/github_leonbay_testi-ranch/moveable-aliases/master/paths/functions/daily-to-history",
-             "https://source.developers.google.com/projects/leo-test-env-1/repos/github_leonbay_testi-ranch/moveable-aliases/master/paths/functions/currency-history-to-csv",
-             "https://source.developers.google.com/projects/leo-test-env-1/repos/github_leonbay_testi-ranch/moveable-aliases/master/paths/functions/CovidHistoryDatatoBigQueryTriggerFunction",
+             "https://source.developers.google.com/projects/leo-test-env-1/repos/github_leonbay_testi-ranch/moveable-aliases/master/paths/functions/currency-history-to-csv"]
+}
+
+variable "source_repository_url2" {
+  type = list(string)
+  default = ["https://source.developers.google.com/projects/leo-test-env-1/repos/github_leonbay_testi-ranch/moveable-aliases/master/paths/functions/CovidHistoryDatatoBigQueryTriggerFunction",
              "https://source.developers.google.com/projects/leo-test-env-1/repos/github_leonbay_testi-ranch/moveable-aliases/master/paths/functions/LatestCovidDatatoBigQueryTriggerFunction"]
 }
 
 variable "function_name" {
   type = list(string)
-  default = ["bq-data-transfer", "currencyhistory", "delete-func", "todayscurrencies", "daily-to-history", "currency-history-to-csv", "CovidHistoryDatatoBigQueryTriggerFunction", "LatestCovidDatatoBigQueryTriggerFunction"]
+  default = ["bq-data-transfer", "currencyhistory", "delete-func", "todayscurrencies", "daily-to-history", "currency-history-to-csv"]
+}
+
+variable "function_name2" {
+  type = list(string)
+  default = ["CovidHistoryDatatoBigQueryTriggerFunction", "LatestCovidDatatoBigQueryTriggerFunction"]
 }
 
 variable "project" {}
