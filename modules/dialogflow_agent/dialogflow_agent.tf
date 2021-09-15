faebf44a254a47bc1a5548a22357ceeff3893ceb
LeTiina <tiina.j.le@gmail.com>
2021-09-15 10:37:16 +0300

    dialogflow

--- modules/dialogflow_agent/dialogflow_agent.tf
+++ modules/dialogflow_agent/dialogflow_agent.tf
@@ -15,30 +15,3 @@ resource "google_dialogflow_fulfillment" "basic_fulfillment" {
             uri      = "https://us-central1-loppuprojekti-325208.cloudfunctions.net/dialogfunction"
     }
 }
-
-resource "google_dialogflow_intent" "MLdataForRoboBob" {
-  project = var.project_id
-  depends_on = [google_dialogflow_agent.RoboBob]
-  display_name = "MLdataForRoboBob"
-  webhook_state = "WEBHOOK_STATE_ENABLED"
-  priority = 1
-  training_phrases {
-     parts {
-         text = "Japan situation on 5.10.2021"
-     }
-     parts {
-         text = "I want to go Sweden next Friday"
-     }
-     repeat_count = 1
-  }
-
-  parameters {
-    id          = "geo-country"
-    entity_type = "projects/loppuprojekti-325208/locations/RoboBob/agents/MLdataForRoboBob/entityTypes/sys.geo-country"
-  }
-
-  parameters{
-    id = "date-time"
-    entity_type = "projects/loppuprojekti-325208/locations/RoboBob/agents/MLdataForRoboBob/entityTypes/sys.date-time"
-  }
-}
