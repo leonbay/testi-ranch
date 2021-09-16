5832491d7e510ec5e904753856934e607b328d50
gitmikko <mikontreeniacco@gmail.com>
2021-09-16 09:57:36 +0300

    päivitetty funktioita

--- functions/bq-data-transfer/main.py
+++ functions/bq-data-transfer/main.py
@@ -1,24 +1,32 @@
 from google.cloud import bigquery
 from google.cloud import storage
+from google.cloud import logging
 
 def upload_to_bq(request):
+    try:
+        client = bigquery.Client()
 
-    client = bigquery.Client()
+        table_id = "leo-test-env-1.testidata.Currency-data"
 
-    table_id = "leo-test-env-1:Covid_Currency.Currency-data"
+        job_config = bigquery.LoadJobConfig(
+            
+            skip_leading_rows=1
+        )
+        job_config.write_disposition = 'WRITE_TRUNCATE'
+        uri = "gs://currency_csv_bucket-test/history.csv"
 
-    job_config = bigquery.LoadJobConfig(
-        
-        skip_leading_rows=1
-    )
-    job_config.write_disposition = 'WRITE_TRUNCATE'
-    uri = "gs://currency_csv_bucket-test/history.csv"
+        load_job = client.load_table_from_uri(
+            uri, table_id, job_config=job_config
+        )  
 
-    load_job = client.load_table_from_uri(
-        uri, table_id, job_config=job_config
-    )  
+        load_job.result()  
 
-    load_job.result()  
-
-    destination_table = client.get_table(table_id)  
-    print("Loaded {} rows.".format(destination_table.num_rows))
+        destination_table = client.get_table(table_id)  
+        print("Loaded {} rows.".format(destination_table.num_rows))
+        return 'success'
+    except:
+        logger_name = "logger_1"
+        logging_client = logging.Client()
+        logger = logging_client.logger(logger_name)
+        logger.log_text("bq-transfer function failed!", severity="ERROR")
+        return 'failed'
--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -1,6 +1,6 @@
 variable "name" {
   type        = list(string)
-  default     = ["cloud-function-final-project-test", "currency-raw-data-json-test", "currency_csv_bucket-test", "covid-archive-test", "covid-raw-data-test"]
+  default     = ["currency-archive-test", "currency-raw-data-json-test", "currency_csv_bucket-test", "covid-archive-test", "covid-raw-data-test"]
 }
 #vaihda nimet vikaan versioon
 
